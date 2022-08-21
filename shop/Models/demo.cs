9bad3b1d1e30e0327405522dae76a85f2df6717c
Sravan-13 <sravan.2018.ece@anits.edu.in>
2022-08-21 11:26:24 +0530

    Add project files.

---
+++ shop/Models/demo.cs
@@ -0,0 +1,11 @@
+﻿namespace shop.Models
+{
+    public class demo
+    {
+        public int? id { get; set; }
+
+        public string? name { get; set; }
+
+        public string address { get; set; }
+    }
+}
